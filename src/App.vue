32720e764344ed2437a8d9f0bc3f53e2b527614d
Jackson Ricardo Schroeder <xereda@Gis-Mac-mini.local>
2020-09-03 09:24:29 -0300

    add repository files

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="hero is-dark">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Vue Composition API
+        </h1>
+        <h2 class="subtitle">God save The <strong>Evan You</strong>!</h2>
+      </div>
+    </div>
+  </section>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <h4 class="title is-4">Options API</h4>
+          <options-api />
+        </div>
+        <div class="column">
+          <h4 class="title is-4">Composition API</h4>
+          <composition-api />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import OptionsApi from './components/options-api.vue';
+import CompositionApi from './components/composition-api.vue';
+
+export default {
+  name: 'App',
+  components: {
+    OptionsApi,
+    CompositionApi,
+  },
+};
+</script>
---
+++ src/components/options-api.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="button is-danger" @click="increase">
+    increasing {{ count }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "SimpleIncreaseOptionsAPI",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increase() {
+      this.count++;
+    }
+  }
+};
+</script>
