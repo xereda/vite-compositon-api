920b1f6851d87dd72bb35df96a49bd79ba9fe079
Jackson Ricardo Schroeder <xereda@Gis-Mac-mini.local>
2020-09-09 15:23:24 -0300

    fix grammar error

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -5,7 +5,7 @@
         <strong>Vue Composition API</strong> by
         <a href="https://www.twitter.com/xereda">Jackson Ricardo Schroeder</a>.
       </p>
-      <p>God save The <strong>Evan You</strong> (and your team)!</p>
+      <p>God save The <strong>Evan You</strong> (and his team)!</p>
     </div>
   </footer>
 </template>
