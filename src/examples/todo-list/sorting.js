4a05753580d5e08bd84afb20d0a53446915e12ba
Jackson Ricardo Schroeder <xereda@Gis-Mac-mini.local>
2020-09-09 12:22:16 -0300

    refact sorting function

--- src/examples/todo-list/sorting.js
+++ src/examples/todo-list/sorting.js
@@ -1,7 +1,8 @@
 import { computed } from 'vue';
 
-export function useSorting(list) {
-  return computed(() =>
+const useSorting = list =>
+  computed(() =>
     [...list.value].sort((a, b) => (a.description > b.description ? 1 : -1)),
   );
-}
+
+export { useSorting };
